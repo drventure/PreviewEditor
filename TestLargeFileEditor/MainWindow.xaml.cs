58d30cb352feb8206ef7b8342f5433f3a93acfd9
Darin Higgins <dhiggins@worksoft.com>
2022-12-18 17:36:08 -0600

    Added testbed project for experimenting with using AvalonEdit as a huge file viewer. Ie paging in a window of the file at a time, so that the whole file is never loaded. Would have to be readonly because edits wouldn't work, but all the syntax highlighting etc would. This version has all the basic navigation and paging functional.

---
+++ TestLargeFileEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestLargeFileEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileWindow _window = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var opn = new OpenFileDialog();
+            opn.InitialDirectory = @"C:\Dev\Darin\CodePreviewEditor";
+            var r = opn.ShowDialog();
+            if (r == System.Windows.Forms.DialogResult.OK)
+            {
+                var file = opn.FileName;
+
+                _window?.Dispose();
+                _window = new FileWindow(file, Editor);
+            }
+        }
+    }
+}
