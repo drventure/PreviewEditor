29f1202d38b0f6a9c71d09f1d03df16b6bc7dbc5
Darin Higgins <dhiggins@worksoft.com>
2023-09-03 18:44:12 -0500

    Removed the PreviewEditorHostOld.

--- PreviewEditorHostOld2/MainForm.cs
+++ PreviewEditorHostOld2/MainForm.cs
@@ -21,10 +21,7 @@ namespace PreviewEditorHost
 
         private Button btnSelect;
         private Panel pnlPreviewHost;
-        private Button btnViewLargeFile1;
-        private Button btnViewLargeFile2;
-        private Button btnViewLargeJSON;
-        private Button btnHexFile;
+        private Button btnViewFile;
 
         /// <summary>
         /// Required designer variable.
@@ -54,10 +51,7 @@ namespace PreviewEditorHost
         {
             this.btnSelect = new System.Windows.Forms.Button();
             this.pnlPreviewHost = new System.Windows.Forms.Panel();
-            this.btnViewLargeFile1 = new System.Windows.Forms.Button();
-            this.btnViewLargeFile2 = new System.Windows.Forms.Button();
-            this.btnViewLargeJSON = new System.Windows.Forms.Button();
-            this.btnHexFile = new System.Windows.Forms.Button();
+            this.btnViewFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSelect
@@ -81,55 +75,22 @@ namespace PreviewEditorHost
             this.pnlPreviewHost.Size = new System.Drawing.Size(1242, 583);
             this.pnlPreviewHost.TabIndex = 1;
             // 
-            // btnViewLargeFile1
+            // btnViewFile
             // 
-            this.btnViewLargeFile1.Location = new System.Drawing.Point(12, 66);
-            this.btnViewLargeFile1.Name = "btnViewLargeFile1";
-            this.btnViewLargeFile1.Size = new System.Drawing.Size(172, 48);
-            this.btnViewLargeFile1.TabIndex = 2;
-            this.btnViewLargeFile1.Text = "Large File 1";
-            this.btnViewLargeFile1.UseVisualStyleBackColor = true;
-            this.btnViewLargeFile1.Click += new System.EventHandler(this.btnViewLargeFile1_Click);
-            // 
-            // btnViewLargeFile2
-            // 
-            this.btnViewLargeFile2.Location = new System.Drawing.Point(12, 120);
-            this.btnViewLargeFile2.Name = "btnViewLargeFile2";
-            this.btnViewLargeFile2.Size = new System.Drawing.Size(172, 48);
-            this.btnViewLargeFile2.TabIndex = 3;
-            this.btnViewLargeFile2.Text = "Large File 2";
-            this.btnViewLargeFile2.UseVisualStyleBackColor = true;
-            this.btnViewLargeFile2.Click += new System.EventHandler(this.btnViewLargeFile2_Click);
-            // 
-            // btnViewLargeJSON
-            // 
-            this.btnViewLargeJSON.Location = new System.Drawing.Point(12, 174);
-            this.btnViewLargeJSON.Name = "btnViewLargeJSON";
-            this.btnViewLargeJSON.Size = new System.Drawing.Size(172, 48);
-            this.btnViewLargeJSON.TabIndex = 4;
-            this.btnViewLargeJSON.Text = "Large JSON";
-            this.btnViewLargeJSON.UseVisualStyleBackColor = true;
-            this.btnViewLargeJSON.Click += new System.EventHandler(this.btnViewLargeJSON_Click);
-            // 
-            // btnHexFile
-            // 
-            this.btnHexFile.Location = new System.Drawing.Point(12, 228);
-            this.btnHexFile.Name = "btnHexFile";
-            this.btnHexFile.Size = new System.Drawing.Size(172, 48);
-            this.btnHexFile.TabIndex = 5;
-            this.btnHexFile.Text = "Hex File";
-            this.btnHexFile.UseVisualStyleBackColor = true;
-            this.btnHexFile.Click += new System.EventHandler(this.btnHexFile_Click);
+            this.btnViewFile.Location = new System.Drawing.Point(12, 66);
+            this.btnViewFile.Name = "btnViewFile";
+            this.btnViewFile.Size = new System.Drawing.Size(172, 48);
+            this.btnViewFile.TabIndex = 2;
+            this.btnViewFile.Text = "View File";
+            this.btnViewFile.UseVisualStyleBackColor = true;
+            this.btnViewFile.Click += new System.EventHandler(this.btnViewFile_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1453, 607);
-            this.Controls.Add(this.btnHexFile);
-            this.Controls.Add(this.btnViewLargeJSON);
-            this.Controls.Add(this.btnViewLargeFile2);
-            this.Controls.Add(this.btnViewLargeFile1);
+            this.Controls.Add(this.btnViewFile);
             this.Controls.Add(this.pnlPreviewHost);
             this.Controls.Add(this.btnSelect);
             this.Name = "MainForm";
@@ -143,6 +104,7 @@ namespace PreviewEditorHost
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            //fileExplorer.PopulateView(@"C:\");
         }
 
         private void btnSelect_Click(object sender, EventArgs e)
@@ -152,24 +114,29 @@ namespace PreviewEditorHost
             var r = opn.ShowDialog();
             if (r == DialogResult.OK)
             {
-                View(opn.FileName);
+                var file = opn.FileName;
+
+                var prv = new PreviewEditor.PreviewEditorControl();
+                prv.Dock = DockStyle.Fill;
+
+                pnlPreviewHost.Controls.Clear();
+                pnlPreviewHost.Controls.Add(prv);
+                prv.Visible = true;
+                prv.Refresh();
+                
+                //in a real host, the PreviewHandlerBase interface called IPreviewHandlerVisuals is called
+                //which then forwards the colors on to these methods on the control
+                prv.SetBackgroundColor(Color.FromArgb(0x1e, 0x1e, 0x1e));
+                prv.SetTextColor(Color.WhiteSmoke);
+
+                prv.DoPreview<string>(file);
             }
         }
 
-        private void btnViewLargeFile1_Click(object sender, EventArgs e)
+        private void btnViewFile_Click(object sender, EventArgs e)
         {
-            View("..\\..\\..\\LargeTextFile1.txt");
-        }
+            var file = "..\\..\\..\\..\\LargeTextFile.txt";
 
-
-        private void btnViewLargeFile2_Click(object sender, EventArgs e)
-        {
-            View("..\\..\\..\\LargeTextFile2.txt");
-        }
-
-
-        private void View(string file)
-        {
             var prv = new PreviewEditor.PreviewEditorControl();
             prv.Dock = DockStyle.Fill;
 
@@ -184,18 +151,6 @@ namespace PreviewEditorHost
             prv.SetTextColor(Color.WhiteSmoke);
 
             prv.DoPreview<string>(file);
-        }
-
-
-        private void btnViewLargeJSON_Click(object sender, EventArgs e)
-        {
-            View("..\\..\\..\\VeryLargeJSON.json");
-        }
-
-
-        private void btnHexFile_Click(object sender, EventArgs e)
-        {
-            View("..\\..\\..\\SampleBinaryFile.bin");
         }
     }
 }
